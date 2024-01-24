72a6d4a019cf78481b038183a1102ab3c73ed3d6
Kuldeep Sahrma <kldpsh77@gmail.com>
2024-01-24 10:57:04 +0530

    Summary Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Layout/Header/Header';
+import Summary from './components/Layout/Summary/Summary';
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
+      <Summary/>
     </React.Fragment>
   );
 }
